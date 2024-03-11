246b8e6aecd58b5be31b167c8d0d3bd1b48ca23a
lucasbueno20 <lucasbueno783@gmail.com>
2024-03-10 23:58:24 -0300

    adjustments

--- app/src/main/java/com/lucasbueno/thenamegamewt/utils/MyGlideModule.kt
+++ app/src/main/java/com/lucasbueno/thenamegamewt/utils/MyGlideModule.kt
@@ -1,4 +1,31 @@
 package com.lucasbueno.thenamegamewt.utils
 
-class MyGlideModule {
+import android.content.Context;
+
+
+import androidx.annotation.NonNull;
+
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+import java.io.InputStream;
+import okhttp3.OkHttpClient;
+import java.util.concurrent.TimeUnit
+
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule() {
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val client = OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
+        val factory = OkHttpUrlLoader.Factory(client)
+        glide.registry.replace(GlideUrl::class.java, InputStream::class.java, factory)
+    }
 }
