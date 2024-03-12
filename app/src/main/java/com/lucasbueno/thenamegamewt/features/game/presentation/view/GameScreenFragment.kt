843367a150125d9a9e4aee344b2db9a0f1b20968
lucasbueno20 <lucasbueno783@gmail.com>
2024-03-12 09:22:07 -0300

    setup project

--- app/src/main/java/com/lucasbueno/thenamegamewt/features/game/presentation/view/GameScreenFragment.kt
+++ app/src/main/java/com/lucasbueno/thenamegamewt/features/game/presentation/view/GameScreenFragment.kt
@@ -3,16 +3,12 @@ package com.lucasbueno.thenamegamewt.features.game.presentation.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.lucasbueno.thenamegamewt.R
 import com.lucasbueno.thenamegamewt.databinding.FragmentGameBinding
-import com.lucasbueno.thenamegamewt.features.game.presentation.viewmodel.GameScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GameScreenFragment : Fragment(R.layout.fragment_game) {
-
-    private val viewModel: GameScreenViewModel by viewModels()
 
     private var _binding: FragmentGameBinding? = null
 
--- app/src/main/java/com/lucasbueno/thenamegamewt/features/onboarding/presentation/view/OnboardingFragment.kt
+++ app/src/main/java/com/lucasbueno/thenamegamewt/features/onboarding/presentation/view/OnboardingFragment.kt
@@ -3,16 +3,12 @@ package com.lucasbueno.thenamegamewt.features.onboarding.presentation.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.lucasbueno.thenamegamewt.R
 import com.lucasbueno.thenamegamewt.databinding.FragmentOnboardingBinding
-import com.lucasbueno.thenamegamewt.features.onboarding.presentation.viewmodel.OnboardingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
-
-    private val onboardingViewModel: OnboardingViewModel by viewModels()
 
     private var _binding: FragmentOnboardingBinding? = null
     private val binding get() = _binding!!
