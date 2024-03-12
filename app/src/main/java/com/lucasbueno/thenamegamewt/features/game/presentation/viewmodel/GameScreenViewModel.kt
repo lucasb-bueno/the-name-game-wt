040d64a992db5a01e96cb5226cccbb8f5a8852cd
lucasbueno20 <lucasbueno783@gmail.com>
2024-03-12 11:05:08 -0300

    setup project

--- app/src/main/java/com/lucasbueno/thenamegamewt/features/game/presentation/viewmodel/GameScreenViewModel.kt
+++ app/src/main/java/com/lucasbueno/thenamegamewt/features/game/presentation/viewmodel/GameScreenViewModel.kt
@@ -1,22 +1,11 @@
 package com.lucasbueno.thenamegamewt.features.game.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.lucasbueno.thenamegamewt.features.game.domain.model.GameDataItem
-import com.lucasbueno.thenamegamewt.features.game.domain.usecase.GetGameDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GameScreenViewModel @Inject constructor(
-    private val useCase: GetGameDataUseCase
 ) : ViewModel() {
 
 
